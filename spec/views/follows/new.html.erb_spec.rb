a7e76fa61eb61fa320689e124ee2d46fb3f88cfc
jakovh1 <1jakov.1horvat@gmail.com>
2025-09-09 19:51:24 +0200

    Add Follow Scaffold
      - Set Associations
      - Setup Migration, Indexes and Constraints
      - Run Migration

---
+++ spec/views/follows/new.html.erb_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe "follows/new", type: :view do
+  before(:each) do
+    assign(:follow, Follow.new(
+      follower: nil,
+      followee: nil,
+      status: "MyString"
+    ))
+  end
+
+  it "renders new follow form" do
+    render
+
+    assert_select "form[action=?][method=?]", follows_path, "post" do
+
+      assert_select "input[name=?]", "follow[follower_id]"
+
+      assert_select "input[name=?]", "follow[followee_id]"
+
+      assert_select "input[name=?]", "follow[status]"
+    end
+  end
+end
